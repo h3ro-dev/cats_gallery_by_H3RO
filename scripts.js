e5e9db0217f4d48be79eb850bae27cfde2d0ded3
CryptoJym <131711520+CryptoJym@users.noreply.github.com>
2025-02-15 20:47:18 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Using ES Module syntax
+
+// Initialization for the tutorial page
+document.addEventListener('DOMContentLoaded', () => {
+  // Tutorial page initialization code can be added here.
+});
